3f46722f637a288b7f8c77169e55a843d1d438cf
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-01 10:41:23 -0300

    intro

--- my-new-portfolio/src/App.js
+++ my-new-portfolio/src/App.js
@@ -3,11 +3,11 @@ import { Helmet } from 'react-helmet';
 import Navbar from "./Components/Navbar/Navbar"
 import './App.css';
 import Intro from "./Components/Intro/Intro"
-import About from "./Components/About/About"
-import Experience from "./Components//Experience/Experience";
-import Projects from "./Components/Projects/Projects";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
+// import About from "./Components/About/About"
+// import Experience from "./Components//Experience/Experience";
+// import Projects from "./Components/Projects/Projects";
+// import Contact from "./Components/Contact/Contact";
+// import Footer from "./Components/Footer/Footer";
 
 
 function App() {
@@ -22,11 +22,11 @@ function App() {
       </Helmet>
       <Navbar/>
       <Intro />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-      <Footer />
+      {/* <About /> */}
+      {/* <Experience /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
