7fab718bc38cec612360da8c9e30a333136a93e5
Marcelo Amaral Alves <marceloaa@live.com>
2024-05-03 12:54:26 -0300

    new project

--- my-new-portfolio/src/Components/Projects/ProjectsCard.jsx
+++ my-new-portfolio/src/Components/Projects/ProjectsCard.jsx
@@ -13,7 +13,7 @@ export const ProjectCard = ({ title, description, imgUrl, link}) => {
             {title === "Game of Codes" && (
               <a href="https://www.game-of-codes.pro/" className="button i-button">Live Demo</a>
             )}
-            {title === "Shoes App" && (
+            {title === "3D Content in iOS" && (
               <a href="https://www.linkedin.com/feed/update/urn:li:activity:7191889677941948416/" className="button i-button">Live Demo</a>
             )}
           </div>
